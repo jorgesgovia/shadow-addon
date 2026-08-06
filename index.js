1df3808de5c553ac745e2c76dfa4b1a12d79a689
Jorge Segovia <jorgesegovia@MacBook-Air-de-Jorge.local>
2026-08-06 02:58:10 -0600

    Fix streamlog syntax

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ episode: index + 1,
 
 builder.defineStreamHandler(async ({ id }) => {
 
-console.log(STREAM PEDIDO:", id);
+console.log("STREAM PEDIDO:", id);
 
   const number = id.match(/flashman-(\d+)/);
 
